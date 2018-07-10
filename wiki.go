1c1c831abad01d283760ced802507ae3890c75b5
James Wilson <wilsjame@oregonstate.edu>
2018-07-09 20:34:42 -0700

    add url validation

--- wiki.go
+++ wiki.go
@@ -5,7 +5,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
+	"errors"
 )
+
+// Validation expression.
+var validPath = regexp.MustCompile("^/(edit|save|view|)/([a-zA-Z0-9]+)$")
 
 // Template caching.
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
@@ -23,6 +28,16 @@ type Page struct {
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+// Uses 'validPath' expression to validate path and extract the page title.
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil // The title is second subexpression.
 }
 
 func loadPage(title string) (*Page, error) {
@@ -43,7 +58,10 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 // Allow user to view a wiki page.
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -55,7 +73,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 // Loads the page (or, if it doesn't exist, create an empty 'page'
 // struct), and displays an HTML form.
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -65,10 +86,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 
 // Handle submission of forms located on the edit pages.
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	err = p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
